6b67b36f5c1bb4af79f6efeb266a4b1f2ee76752
Mohadese <mohadese.zare69@gmail.com>
2022-04-20 13:11:32 +0430

    Create valid password in user

--- UserApp/serializers.py
+++ UserApp/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import  User
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
       model = User
       fields = ['username', 'password', 'email', 'first_name', 'last_name', 'nationalCode', 'school_name', 'semat', 'groups']
+
+  def validate(self, attrs):
+      attrs["password"] = make_password(attrs["password"])
+      return attrs
