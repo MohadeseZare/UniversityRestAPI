8f2cb5b0a1da8623b35e900f0deac4d3473acb2f
Mohadese <mohadese.zare69@gmail.com>
2022-04-20 11:53:00 +0430

    update UserApp

--- UserApp/serializers.py
+++ UserApp/serializers.py
@@ -4,18 +4,4 @@ from .models import  User
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
       model = User
-      fields = "__all__"
-
-
-
-'''
-class ChangePasswordSerializer(serializers.Serializer):
-    model = User
-
-    """
-    Serializer for password change endpoint.
-    """
-    old_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
-    
-'''
+      fields = ['username', 'password', 'first_name', 'last_name', 'nationalCode', 'school_name', 'semat']
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,81 +1,13 @@
-from django.shortcuts import render
-from django.contrib.auth import get_user_model
 from rest_framework import viewsets, permissions
-from rest_framework.generics import UpdateAPIView
 from .serializers import UserSerializer
 from .models import User
-from rest_framework import status
-from rest_framework.response import Response
-from django.contrib.auth import update_session_auth_hash
 
-'''
-class ChangePasswordView(UpdateAPIView):
-    """
-    An endpoint for changing password.
-    """
-    serializer_class = ChangePasswordSerializer
-    model = User
-    permission_classes = (permissions.IsAuthenticated,)
 
-    def get_object(self, queryset=None):
-        obj = self.request.user
-        return obj
-
-    def update(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            # Check old password
-            if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-            # set_password also hashes the password that the user will get
-            self.object.set_password(serializer.data.get("new_password"))
-            self.object.save()
-            response = {
-                'status': 'success',
-                'code': status.HTTP_200_OK,
-                'message': 'Password updated successfully',
-                'data': []
-            }
-
-            return Response(response)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-'''
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     queryset =User.objects.all()
     serializer_class = UserSerializer
 
-   # def get_object(self):
-    #    return self.request.user
-
-    #def list(self, request, *args, **kwargs):
-    #   return self.retrieve(request, *args, **kwargs)
-    '''
-    def get_permissions(self):
-        """
-        Instantiates and returns the list of permissions that this view requires.
-        """
-        if self.action == 'update':
-            permission_classes = [permissions.IsAuthenticated]
-        else:
-            permission_classes = [permissions.IsAdminUser]
-        return [permission() for permission in permission_classes]
-
-
-
-
-    def create(self, request):
-        nationalCode = request.POST['NationalCode']
-        lastname = request.POST['LastName']
-        firstname = request.POST['FirstName']
-        user = get_user_model().objects.create_user(nationalCode, '', nationalCode)
-        user.last_name = lastname
-        user.first_name = firstname
-        user.save()
-'''
 
 
 
